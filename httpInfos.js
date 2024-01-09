5aa785192d3e50fea0bb9a171b3c2515de0f675a
Jó Sales <32043542+josalesmj@users.noreply.github.com>
2024-01-09 17:04:20 -0300

    Adds statusCode to response

--- httpInfos.js
+++ httpInfos.js
@@ -1,15 +1,16 @@
 module.exports = function (RED) {
     "use strict";
-    RED.httpNode.get("/http-api/swagger.json", (req, res) => {
+    RED.httpNode.get("/http-api/nodes-json", (req, res) => {
         const nodesInfos = [];
         RED.nodes.eachNode(node => {
             if (node.type === "http in") {
                 nodesInfos.push({
+                    id: node.id,
                     path: node.url,
                     method: node.method,
                 });
             }
         });
-        res.json(nodesInfos);
+        res.status(200).json(nodesInfos);
     });
 };
